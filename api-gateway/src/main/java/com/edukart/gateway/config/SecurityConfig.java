a967e3f55607c1d0ef323e650f472077f5df13e3
AdityaByte <adityapawar4914@gmail.com>
2025-08-03 13:31:28 +0530

    feat: implemented role based authorization with keycloak

--- api-gateway/src/main/java/com/edukart/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/edukart/gateway/config/SecurityConfig.java
@@ -2,9 +2,18 @@ package com.edukart.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,9 +26,25 @@ public class SecurityConfig {
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/eureka/**") // Through this route we load the static files so permitting it without authorization.
                         .permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/product").permitAll()
+                        .pathMatchers(HttpMethod.POST, "/api/product").hasRole("CUSTOMER")
                         .anyExchange()
                         .authenticated())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())))
                 .build();
+    }
+
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            JwtAuthConvertor delegate = new JwtAuthConvertor();
+            Collection<GrantedAuthority> authorities = delegate.convert(jwt);
+            return Flux.fromIterable(authorities);
+        });
+        return converter;
     }
 }
