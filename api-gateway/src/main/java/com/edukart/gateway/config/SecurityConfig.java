cf880216ce3bd7b4acbe4c27519c5fc250c12edb
AdityaByte <adityapawar4914@gmail.com>
2025-08-03 12:06:22 +0530

    feat: added keycloak server for authentication and authorization

---
+++ api-gateway/src/main/java/com/edukart/gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.edukart.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+        return security
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**") // Through this route we load the static files so permitting it without authorization.
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
+                .build();
+    }
+}
